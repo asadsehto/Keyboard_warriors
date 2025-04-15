f057d507e339925de04b6869093026b0b9835149
Karan Munreja <153481061+karanmunreja@users.noreply.github.com>
2025-04-15 07:08:17 -0700

    Add files via upload

--- PROJECT/script.js
+++ PROJECT/script.js
@@ -5,6 +5,7 @@ const content = document.querySelector('section');
 toggle_btn.onclick = function() {
     nav.classList.toggle('hide');
     content.classList.toggle('expand');
+    
 };
 
 const body = document.querySelector('body');
@@ -17,4 +18,30 @@ bgModeBtn.onclick = function(){
     bgModeIcon.classList.toggle("fa-sun-o");
     bgModeIcon.classList.toggle("fa-moon-o");
     sectionHeader.classList.toggle("dark-mode");
+}
+const images = [ 
+    'img1.jpg', 
+    'bg1.jpg', 
+    'bg2.jpg' 
+]; 
+ 
+let currentIndex = 0; 
+ 
+function changeImage() { 
+    const slideshow = document.getElementById('slideshow'); 
+    currentIndex = (currentIndex + 1) % images.length; 
+    slideshow.style.opacity = 0; // Fade out 
+    setTimeout(() => { 
+        slideshow.src = images[currentIndex]; 
+        slideshow.style.opacity = 1; // Fade in 
+    }, 1000); // Wait for fade out to finish 
+} 
+
+function feedback(){
+    // if (document.getElementById('txtarea').value ){
+    document.getElementById("subm").style.display="none";
+    document.getElementById("txtarea").style.display="none";
+    document.getElementById("txt").textContent="THANKS FOR YOUR FEEDBACK!";
+    // }
+    // button
 }
